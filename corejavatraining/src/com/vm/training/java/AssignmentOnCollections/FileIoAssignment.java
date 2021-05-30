fb4219a890bb22cb312e7706c1419268e39131c9
sasi chowdary <sasikumargadiparthi319@gmail.com>
2021-05-30 18:15:48 +0530

    core java problems

--- corejavatraining/src/com/vm/training/java/AssignmentOnCollections/FileIoAssignment.java
+++ corejavatraining/src/com/vm/training/java/AssignmentOnCollections/FileIoAssignment.java
@@ -6,26 +6,80 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class FileIoAssignment
 {
-	public static void main(String[] args) throws IOException 
+	/**
+	 * This methos is used to print the list by removing the delhi from the list
+	 * @param lhs
+	 */
+	static void deleteTheSpecificElement(Set<String> lhs)
+	{
+		String s="Delhi";
+		lhs.remove(s);
+		for (String string : lhs) {
+			System.out.println(string);
+		}
+	}
+	/**
+	 * This method is used to print the state name starts with 'K'
+	 * @param lhs
+	 */
+	static void displayTheStateNameStartWithK(Set<String> lhs)
+	{
+		for(String s:lhs)
+		{
+			if(s.startsWith("K"))
+			{
+				System.out.println(s);
+			}
+		}
+	}
+	/**
+	 * This method is used to print the list in sorted order
+	 * @param lhs
+	 */
+	static void sortTheList(Set<String> lhs)
+	{
+		TreeSet<String> ts=new TreeSet<String>(lhs);
+		System.out.println(ts);
+	}
+	public static void main(String[] args) throws IOException
 	{
 		FileReader fr=new FileReader("F:\\Personal\\valuemomentum\\Assignments\\States.txt");
 		BufferedReader br=new BufferedReader(fr);
-		Set<BufferedWriter> lhs=new LinkedHashSet<BufferedWriter>();
+		Set<String> lhs=new LinkedHashSet<String>();
 		
 		String readlines=br.readLine();
 		while(readlines!=null)
 		{
-			System.out.println(readlines);
+			
+			lhs.add(readlines);
 			readlines=br.readLine();
-			//lhs.add(bufferedReader.write(readlines));
-		}		
+		}	
+		System.out.println("============Data that was present int the LinkedHashSet============");
+		int count=0;
+		for (String s : lhs)
+		{
+			//System.out.println(s);
+			count++;
+		}
+		System.out.println("==========No of states that was present in the list============");
+		System.out.println(count);
 		
-
+		System.out.println("==========Remove delhi from the List and display============");
+		deleteTheSpecificElement(lhs);
+		 
+		System.out.println("==========print the state name starting with 'k'  ============");
+		displayTheStateNameStartWithK(lhs);
+		
+		System.out.println("=========print the list in sorted order============");
+		sortTheList(lhs);
+		
 	}
 
 }
--- corejavatraining/src/com/vm/training/java/AssignmentOnCollections/TestPatient.java
+++ corejavatraining/src/com/vm/training/java/AssignmentOnCollections/TestPatient.java
@@ -1,3 +1,4 @@
+
 package com.vm.training.java.AssignmentOnCollections;
 
 import java.util.ArrayList;
